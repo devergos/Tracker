8f66ca0417073a5b563589dd6fa7fc6cddca10ec
Dimitris <divergos@gmail.com>
2020-06-23 17:32:04 +0300

    first commit

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-    return <>
-        <Text style={{ fontSize: 48 }}>
-            SignupScreen
-    </Text>
-        <Button title="Go to SignIn" onPress={() => navigation.navigate('Signin')} />
-        <Button title="Go to MainFlow" onPress={() => navigation.navigate('mainFlow')} />
-    </>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return <View style={styles.container}>
+        <Spacer>
+            <Text h4>Sign Up for Tracker</Text>
+        </Spacer>
+        <Input
+            label="Email"
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize='none'
+            autoCorrect={false}
+        />
+        <Spacer />
+        <Input
+            secureTextEntry={true}
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+            autoCapitalize='none'
+            autoCorrect={false}
+        />
+        <Spacer >
+            <Button title="Sign Up" />
+        </Spacer >
+    </View>
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = {
+
+    header: null,
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250
+        // alignItems:'center'
+    }
+});
 
 export default SignupScreen;
